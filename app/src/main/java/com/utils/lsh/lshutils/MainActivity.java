70052b544bf58fa771d1c2eac3fdd7ee7e6bbc37
adonis_lsh <adonis_lsh@163.com>
2017-10-23 15:01:12 +0800

    后期会把一些常用的工具类也封装到里面

---
+++ app/src/main/java/com/utils/lsh/lshutils/MainActivity.java
@@ -0,0 +1,26 @@
+package com.utils.lsh.lshutils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.commons.utils.ObjectUtils;
+
+/**
+ * 对commons-lang3-3.6这个常用的工具类的一个更换包名的操作,为什么会如此无聊的
+ * 去更换包名,因为当你的项目引用commons-lang3-3.6这个工具类时,调用ObjectUtils这个类
+ * 会直接在小米5上面报错,原因就是小米5的rom也引用了这个第三方库,但是没有更改包名导致,他不改
+ * 肯定需要咱们去改.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Object o = new Object();
+        if (ObjectUtils.allNotNull(o)) {
+            Toast.makeText(this, "sdfsadf", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
